ef6cea68e1aee8513fa1de32d8c80164dbd2e67e
mihmin98 <mihmin98@yahoo.com>
2022-05-22 16:32:52 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.10
+
+WORKDIR /code
+
+COPY ./Pipfile ./Pipfile.lock /code/
+RUN python -m pip install --upgrade pip
+RUN pip install pipenv 
+RUN PIPENV_VENV_IN_PROJECT=1 pipenv install --deploy
+
+COPY ./app /code/app
+COPY ./model /code/model
+
+CMD ["pipenv", "run", "uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import numpy as np
+
+class PredictRequest(BaseModel):
+    SepalLengthCm: float
+    SepalWidthCm: float
+    PetalLengthCm: float
+    PetalWidthCm: float
+
+    def get_as_array(self):
+        arr = np.array([self.SepalLengthCm, self.SepalWidthCm, self.PetalLengthCm, self.PetalWidthCm])
+        return arr.reshape(1, -1)
+
+app = FastAPI()
+
+@app.get("/test")
+async def test_get():
+    return {'message': 'Hatz gionule'}
+
+@app.get("/predict")
+async def get_prediction(request: PredictRequest):
+    model = None
+    with open('../model/model.pickle', 'rb') as f:
+        model = pickle.load(f)
+
+    label_encoder = None
+    with open('../model/label_encoder.pickle', 'rb') as f:
+        label_encoder = pickle.load(f)
+
+    prediction = model.predict(request.get_as_array())
+    return label_encoder.inverse_transform(prediction)[0]
+
+    
