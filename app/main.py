5a8c96b8ff113a254efd1f307772487d9880bdcb
mihmin98 <mihmin98@yahoo.com>
2022-05-23 15:42:13 +0300

    Added error logging

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import pickle
 import numpy as np
+
 
 class PredictRequest(BaseModel):
     SepalLengthCm: float
@@ -14,23 +16,31 @@ class PredictRequest(BaseModel):
         arr = np.array([self.SepalLengthCm, self.SepalWidthCm])
         return arr.reshape(1, -1)
 
+
 app = FastAPI()
+
 
 @app.get("/test")
 async def test_get():
-    return {'message': 'Hatz gionule'}
+    return {"message": "Hatz gionule"}
+
 
 @app.get("/predict")
 async def get_prediction(request: PredictRequest):
-    model = None
-    with open('../model/model.pickle', 'rb') as f:
-        model = pickle.load(f)
+    try:
+        model = None
+        with open("../model/model.pickle", "rb") as f:
+            model = pickle.load(f)
 
-    label_encoder = None
-    with open('../model/label_encoder.pickle', 'rb') as f:
-        label_encoder = pickle.load(f)
+        label_encoder = None
+        with open("../model/label_encoder.pickle", "rb") as f:
+            label_encoder = pickle.load(f)
 
-    prediction = model.predict(request.get_as_array())
-    return label_encoder.inverse_transform(prediction)[0]
+        prediction = model.predict(request.get_as_array())
+        predicted_label = label_encoder.inverse_transform(prediction)[0]
 
-    
+    except Exception as e:
+        content = {"message": str(e)}
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=content)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"predicted_label": predicted_label})
